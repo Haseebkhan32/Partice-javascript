c6517de3974b0efae5a979355b300d07f71c0a74
Haseebkhan32 <haseebkhanb321@gmail.com>
2025-11-05 04:33:31 +0500

    Task Complete

---
+++ Partice-javascript-3/script.js
@@ -0,0 +1,89 @@
+
+
+// Tasks- 1 
+
+let string1 = "Cafe";
+console.log("value: " + ""+ string1 +" "+"Type: "+ " " + typeof string1);
+
+let number2 = 50;
+console.log("value: " + ""+ number2 +" "+"Type: "+ " " + typeof number2);
+
+let boolean3 = true;
+console.log("value: " + ""+ boolean3 +" "+"Type: "+ " " + typeof boolean3);
+
+let null4 = null;
+console.log("value: " + ""+ null4 +" "+"Type: "+ " " + typeof null4);
+
+let undefined5 ;
+console.log("value: " + ""+ undefined5 +" "+"Type: "+ " " + typeof undefined5);
+
+let bigint6 = 324240424322;
+console.log("value: " + ""+ bigint6 +" "+"Type: "+ " " + typeof bigint6);
+
+// let symbol7 = symbol("id2") ;
+// let symbol8 = symbol("id2") ;
+// console.log("value: " + ""+ symbol7 +" "+"Type: "+ " " + typeof symbol7);
+
+
+// Task-2
+
+
+let num1 = 10; 
+let num2 = num1;
+num2 = 12;
+console.log(num2);
+console.log(num1);
+console.log("simple as that number are Primitive date type num1 are not changed beacuse is by value date type..")
+
+
+// Tasks-3 
+
+let name = prompt("What is your name");
+
+console.log("Name:" + " " + name + " - " + typeof name);
+
+
+
+
+let age = prompt("What is your age");
+
+let convertage  = Number(age);
+
+console.log("Age:" + " " + convertage + " - " + typeof convertage);
+
+
+
+
+let isStudent = prompt("Your are student in SMIT ? Yes and No ? ");
+
+let convertisStudent = Boolean(isStudent);
+
+console.log("SMIT Student:" + " " + convertisStudent + " - " + typeof convertisStudent);
+
+
+
+
+let Favoritenumber = prompt("What is your Favorite Number");
+
+let fnumberconvert = Number(Favoritenumber);
+
+console.log("Favorite Number:" + " " + fnumberconvert + " - " + typeof fnumberconvert);
+
+
+
+
+let Nickname = String(prompt("What is your NickName"));
+
+let NickNameconvert = String(Nickname);
+
+console.log("Nickname:" + " " + NickNameconvert + " - " + typeof NickNameconvert);
+
+
+let age5  = Number(prompt("you are under age"));
+console.log("learn a new thing Prompt to direct Change" + " " + typeof age5 );
+
+console.log(alert("Wellcome And Thanks To FeedBack "));
+
+
+
+
